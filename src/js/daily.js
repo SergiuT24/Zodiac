b5c0807f15c035ebdb09199c39375086e945871c
Sergiu Tudos <cambino7777@gmail.com>
2024-03-09 14:05:03 +0200

    img check 3

--- src/js/daily.js
+++ src/js/daily.js
@@ -14,7 +14,7 @@ const matchesIds = ["love", "career", "friend"];
 matchesIds.forEach(matchesId => {
 	const imgElement = document.getElementById(matchesId);
 	const randomNumber = Math.floor(Math.random() * 12) + 1;
-	imgElement.src = 'https://horoscopzilei.netlify.app/' + randomNumber + '.svg';
+	imgElement.src = 'https://horoscopzilei.netlify.app/public/icons/match/' + randomNumber + '.svg';
 });
 
 // RANDOM RATING STARS OF MOOD
