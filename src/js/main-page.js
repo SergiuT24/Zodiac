d5b7b46526dbab73c55ba32018b3cf122ca6456d
Sergiu Tudos <cambino7777@gmail.com>
2024-04-07 17:01:32 +0300

    prediction daily pages

--- src/js/main-page.js
+++ src/js/main-page.js
@@ -24,7 +24,7 @@ fetch(`https://aws-horoscope-base.s3.eu-central-1.amazonaws.com/${formattedDate}
 				content: `
 						<div class="popup-body_sign">Zodia: ${signName}</div>
 						<div class="popup-body_day">Horoscopul: ${romanianDate}</div>
-						<div class="popup-body_text">${jsonData[formattedDate][signName]}<br><br><a class="popup-body_link" href="/signs-html/${signName}.html">Află mai multe despre ${signName}</a></div>
+						<div class="popup-body_text">${jsonData[formattedDate][signName]}<br><br><a class="popup-body_link" href="/zodii/${signName.toLowerCase()}.html">Află mai multe despre ${signName}</a></div>
 						`,
 			});
 			signElement.addEventListener('click', () => {
