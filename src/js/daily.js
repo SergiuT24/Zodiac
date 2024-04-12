3750858aecac1bd9c689e2eff26e28984a6da38a
Sergiu Tudos <cambino7777@gmail.com>
2024-04-12 19:00:03 +0300

    compress png, remove dayjs

--- src/js/daily.js
+++ src/js/daily.js
@@ -1,7 +1,11 @@
-import dayjs from 'dayjs';
-
 document.addEventListener('DOMContentLoaded', function () {
-	const formattedDate = dayjs().format('YYYY-MM-DD');
+	const currentDate = new Date();
+
+	const year = currentDate.getFullYear();
+	const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+	const day = String(currentDate.getDate()).padStart(2, '0');
+
+	const formattedDate = `${year}-${month}-${day}`;
 	const options = { year: 'numeric', month: 'long', day: 'numeric' };
 	const romanianDate = new Date().toLocaleDateString('ro-RO', options);
 
--- src/js/main-page.js
+++ src/js/main-page.js
@@ -1,6 +1,11 @@
-import dayjs from 'dayjs';
+const currentDate = new Date();
 
-const formattedDate = dayjs().format('YYYY-MM-DD');
+const year = currentDate.getFullYear();
+const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+const day = String(currentDate.getDate()).padStart(2, '0');
+
+const formattedDate = `${year}-${month}-${day}`;
+
 const options = { year: 'numeric', month: 'long', day: 'numeric' };
 const romanianDate = new Date().toLocaleDateString('ro-RO', options);
 
